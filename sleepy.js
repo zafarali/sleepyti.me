01472022f26d4cfa3dcf435f890b69064ab89c48
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2014-07-28 01:57:47 -0400

    resolved merge conflicts

--- sleepy.js
+++ sleepy.js
@@ -1,10 +1,10 @@
 var CONSTANTS = {
-	MIN_14 : (1000*60*14),
-	MIN_90 : (1000*60*90)
+	FT : (1000*60*14), //FourTeen minutes
+	N : (1000*60*90) //Ninety minutes
 }
 
-
-function format(date){
+//converts the time to a human readable format
+function formatToHuman(date){
 	var hours = date.getHours(),
 			mins	= date.getMinutes();
 			if(mins < 10){
@@ -12,3 +12,32 @@ function format(date){
 			}
 	return hours + ':' +mins;
 }
+
+//converts the time to a date format
+function formatToDate(hours, mins, ampm){
+	var today = new Date(),
+			day = today.getDate(),
+			month = today.getMonth()+1,
+			year = today.getFullYear();
+
+	return new Date(month + ' ' + day + ' '+ year + ' ' + hours + ':' + mins + ' '+ ampm);
+
+}
+
+
+//returns an array of waking times if you sleep at a certain time.
+function wakeUpTimes(bedtime){
+	//+operator converts date time object into miliseconds.
+	//we then add 14 minutes to that to account for 'falling asleep'
+	bedtime = +bedtime + CONSTANTS.FT;
+
+	var waketimes = [];
+
+	for(var i = 0; i < 6; i++){
+		//calculating wake times!
+		//we take bed times and add 90 minute cycles to it
+		waketimes.push(new Date(bedtime + (CONSTANTS.N * (i+1)));
+	}
+	return waketimes;
+}
+
